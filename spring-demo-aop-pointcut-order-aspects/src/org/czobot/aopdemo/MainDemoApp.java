f193662d63bfcb079b41fbfd21fe4d218c5c270c
Stasqq <stachocz@gmail.com>
2022-02-28 21:39:09 +0100

    spring aop - ordering aspects

---
+++ spring-demo-aop-pointcut-order-aspects/src/org/czobot/aopdemo/MainDemoApp.java
@@ -0,0 +1,43 @@
+package org.czobot.aopdemo;
+
+import org.czobot.aopdemo.dao.AccountDAO;
+import org.czobot.aopdemo.dao.MembershipDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get accoutnDAO bean from spring container
+		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// call business method
+		Account account = new Account();
+		accountDAO.addAccount(account, true);
+		
+		accountDAO.doWork();
+		
+		// call the accountDAO getter/setter methods
+		accountDAO.setName("foobar");
+		accountDAO.setServiceCode("gold");
+		
+		String name = accountDAO.getName();
+		String code = accountDAO.getServiceCode();
+		
+		// get membership bean from spring container
+		MembershipDAO membershipDAO = 
+				context.getBean("membershipDAO", MembershipDAO.class);
+		
+		// call membership business method
+		membershipDAO.addAccount();
+		
+		// close context
+		context.close();
+		
+	}
+
+}
---
+++ spring-demo-aop-pointcut-order-aspects/src/org/czobot/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,20 @@
+package org.czobot.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	@Before("org.czobot.aopdemo.aspect.AopPointcuts.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice() {
+		
+		System.out.println("\n=====>>> Executing @Before advice on method");
+		
+	}
+	
+}
