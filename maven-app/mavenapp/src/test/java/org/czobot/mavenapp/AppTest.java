de79e93736d23ee08c531f0c5bcd451d86bcec9e
Stasqq <stachocz@gmail.com>
2022-03-01 18:47:25 +0100

    maven tutorial

---
+++ maven-app/mavenapp/src/test/java/org/czobot/mavenapp/AppTest.java
@@ -0,0 +1,20 @@
+package org.czobot.mavenapp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
