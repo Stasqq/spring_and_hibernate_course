86ca3f3a75505350dc0e0c122048f076c1c9769a
Stasqq <stachocz@gmail.com>
2022-03-02 21:42:10 +0100

    spring security - jdbc database authentication

---
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/org/czobot/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package org.czobot.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
