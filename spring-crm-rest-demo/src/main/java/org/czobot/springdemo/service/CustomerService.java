6efb4cd3dce75bd438afddca5d8cf857c2f0ecd8
Stasqq <stachocz@gmail.com>
2022-03-04 22:25:57 +0100

    spring REST crm - setup

---
+++ spring-crm-rest-demo/src/main/java/org/czobot/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package org.czobot.springdemo.service;
+
+import java.util.List;
+
+import org.czobot.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
