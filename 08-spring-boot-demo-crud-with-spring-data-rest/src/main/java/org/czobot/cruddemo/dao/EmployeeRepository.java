f3c2f3679337ba6575a896f354a46a88199d453d
Stasqq <stachocz@gmail.com>
2022-03-17 19:28:07 +0100

    spring boot - spring data rest

---
+++ 08-spring-boot-demo-crud-with-spring-data-rest/src/main/java/org/czobot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package org.czobot.cruddemo.dao;
+
+import org.czobot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
